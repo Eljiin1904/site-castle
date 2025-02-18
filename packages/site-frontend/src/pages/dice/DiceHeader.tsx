09511c491924675c3a2a8e4b4f561e90650091b5
josue <josue@pidwin.com>
2025-02-18 11:12:34 -0500

    update Dice Header, design is the same for both mobile and desktop, remove conditional and use only one component, remove header title and px 16 or 20 depends of layout

--- packages/site-frontend/src/pages/dice/DiceHeader.tsx
+++ packages/site-frontend/src/pages/dice/DiceHeader.tsx
@@ -1,12 +1,7 @@
 import { Button } from "@client/comps/button/Button";
-import { Conditional } from "@client/comps/conditional/Conditional";
 import { Div } from "@client/comps/div/Div";
 import { SvgCheckCircle } from "@client/svgs/common/SvgCheckCircle";
-import { SvgDice } from "@client/svgs/common/SvgDice";
 import { SvgInfoCircle } from "@client/svgs/common/SvgInfoCircle";
-import { SvgCog } from "@client/svgs/common/SvgCog";
-import { Dropdown } from "@client/comps/dropdown/Dropdown";
-import { PageTitle } from "@client/comps/page/PageTitle";
 import { Dialogs } from "@client/services/dialogs";
 import { VolumeDropdown } from "#app/comps/sounds/VolumeDropdown";
 import { DiceInfoModal } from "#app/modals/dice/DiceInfoModal";
@@ -15,88 +10,33 @@ import { useAppSelector } from "#app/hooks/store/useAppSelector";
 
 export const DiceHeader = () => {
   const mainLayout = useAppSelector((x) => x.style.mainLayout);
-
+  const small = mainLayout === "mobile";
   return (
-    <Div>
-      <Conditional
-        value={mainLayout}
-        mobile={<MobileHeader />}
-        tablet={<NotMobileHeader />}
-        laptop={<NotMobileHeader />}
-        desktop={<NotMobileHeader />}
+    <Div fx position="absolute" top={0} right={0} zIndex={1} px={small ? 20: 16} py={16}>
+    <Div
+      fx
+      align="flex-start"
+      justify="flex-end"
+      gap={8}
+    >
+      <Button
+        kind="quinary"
+        icon={SvgCheckCircle}
+        label="Fairness"
+        onClick={() =>
+          Dialogs.open(
+            "primary",
+            <FairnessSeedModal historyTo="/fairness/dice" />,
+          )
+        }
       />
-    </Div>
-  );
-};
-
-const MobileHeader = () => {
-  return (
-    <Div fx>
-      <PageTitle
-        heading="Dice"
-        icon={SvgDice}
+      <Button
+        kind="quinary"
+        icon={SvgInfoCircle}
+        onClick={() => Dialogs.open("primary", <DiceInfoModal />)}
       />
-      <Div gap={8}>
-        <VolumeDropdown prefix="dice" />
-        <Dropdown
-          type="menu"
-          button={
-            <Button
-              kind="secondary"
-              icon={SvgCog}
-            />
-          }
-          options={[
-            {
-              type: "action",
-              label: "Fairness",
-              iconLeft: SvgCheckCircle,
-              onClick: () =>
-                Dialogs.open(
-                  "primary",
-                  <FairnessSeedModal historyTo="/fairness/dice" />,
-                ),
-            },
-            {
-              type: "action",
-              label: "Rules",
-              iconLeft: SvgInfoCircle,
-              onClick: () => Dialogs.open("primary", <DiceInfoModal />),
-            },
-          ]}
-        />
-      </Div>
+      <VolumeDropdown prefix="dice" />
     </Div>
-  );
-};
-
-const NotMobileHeader = () => {
-  return (
-    <Div fx position="absolute" top={0} right={0} zIndex={1} px={16} py={16}>
-      <Div
-        fx
-        align="flex-start"
-        justify="flex-end"
-        gap={8}
-      >
-        <Button
-          kind="quinary"
-          icon={SvgCheckCircle}
-          label="Fairness"
-          onClick={() =>
-            Dialogs.open(
-              "primary",
-              <FairnessSeedModal historyTo="/fairness/dice" />,
-            )
-          }
-        />
-        <Button
-          kind="quinary"
-          icon={SvgInfoCircle}
-          onClick={() => Dialogs.open("primary", <DiceInfoModal />)}
-        />
-        <VolumeDropdown prefix="dice" />
-      </Div>
-    </Div>
+  </Div>
   );
 };
