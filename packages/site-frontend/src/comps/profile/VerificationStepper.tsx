7870ea878995fbc09a29fa84fe4925ff215b6295
giovanni-pidwin <giovanni@pidwin.com>
2025-02-25 16:44:10 -0500

    Adding initial stepper logic

--- packages/site-frontend/src/comps/profile/VerificationStepper.tsx
+++ packages/site-frontend/src/comps/profile/VerificationStepper.tsx
@@ -1,7 +1,7 @@
 import { useAppSelector } from "#app/hooks/store/useAppSelector";
 import { Conditional } from "@client/comps/conditional/Conditional";
 import { Div } from "@client/comps/div/Div";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getVerificationComp, verificationStepsData } from "./useProfileData";
 import { Vector } from "@client/comps/vector/Vector";
 import { SvgCheckCircle } from "@client/svgs/common/SvgCheckCircle";
@@ -12,6 +12,29 @@ import "./VerificationStepper.scss";
 export const VerificationStepper = () => {
   const layout = useAppSelector((x) => x.style.mainLayout);
   const [step, setStep] = useState(0);
+
+  const kycTier = useAppSelector((x) => x.user.kyc.tier);
+
+  const tierOneSteps = ["zero", "one", "two", "three"] as const;
+  const [kycStep, setKycStep] = useState<number>(0);
+
+  useEffect(() => {
+    setKycStep(getVerificationOneStep({ tier: kycTier }));
+    function getVerificationOneStep({ tier }: { tier: number }): number {
+      let step: number;
+      // if (whole) {
+      //   step = tierOneSteps.indexOf("zero");
+      // } else
+      if (tier < 0.1) {
+        step = tierOneSteps.indexOf("one");
+      } else if (tier < 0.2) {
+        step = tierOneSteps.indexOf("two");
+      } else {
+        step = tierOneSteps.indexOf("three");
+      }
+      return step;
+    }
+  }, [kycTier]);
 
   return (
     <Conditional
