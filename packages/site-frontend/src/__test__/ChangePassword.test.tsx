bf6de66b1d29218218a179cf08c6ef90960a3faa
giovanni-pidwin <giovanni@pidwin.com>
2025-03-02 17:55:19 -0500

    Clean up comments

--- packages/site-frontend/src/__test__/ChangePassword.test.tsx
+++ packages/site-frontend/src/__test__/ChangePassword.test.tsx
@@ -1,24 +1,20 @@
 import { ChangePassword } from "#app/comps/profile/password/ChangePassword";
 import { render, screen, fireEvent, act } from "@testing-library/react";
-// import userEvent from '@testing-library/user-event'
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 
 describe("Profile Change Password Screen", () => {
   it("value input change", async () => {
-    // Render the component
-    render(<ChangePassword />); // Pass the mock as a prop
+    render(<ChangePassword />);
 
     const oldPasswordInput = screen.getByPlaceholderText("Enter Old Password");
     const newPasswordInput = screen.getByPlaceholderText("Enter New Password");
 
-    // Find the Save button
     const saveButton = screen.getByText("Save Changes");
-    expect(saveButton).toBeInTheDocument(); // Correct the assertion
+    expect(saveButton).toBeInTheDocument();
 
     fireEvent.change(oldPasswordInput, { target: { value: "passwordtest" } });
     fireEvent.change(newPasswordInput, { target: { value: "testPassword" } });
 
-    // Assertions for input values
     expect(oldPasswordInput.value).toBe("passwordtest");
     expect(newPasswordInput.value).toBe("testPassword");
 
