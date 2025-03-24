9e3f261ba693213a422e053c65695f160396113d
josue <josue@pidwin.com>
2025-03-24 11:25:18 -0400

    Footer crypto tokens

---
+++ packages/shared-client/src/svgs/crypto/SvgBUSD.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+
+export const SvgBUSD = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 24 24"
+    {...props}
+  >
+
+    <path
+     d="M11.9991 0.0507812L14.9598 3.07372L7.50462 10.5066L4.54395 7.55478L11.9991 0.0507812Z" fill="currentColor"/>
+     <path
+     d="M16.4937 4.53223L19.4543 7.55517L7.50462 19.4691L4.54395 16.5173L16.4937 4.53223Z" fill="currentColor"/>
+     <path
+      d="M3.01048 9.0127L5.97115 12.0356L3.01048 14.9874L0.0498047 12.0356L3.01048 9.0127Z" fill="currentColor"/>
+     <path
+      d="M20.9885 9.0127L23.9492 12.0356L11.9995 23.9496L9.03882 20.9978L20.9885 9.0127Z" fill="currentColor"/>
+  </svg>
+);
