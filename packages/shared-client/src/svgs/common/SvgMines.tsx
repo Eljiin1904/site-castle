71539e7234cb832c7997ee81cd5335526ac6ad0c
josue <josue@pidwin.com>
2025-03-27 17:21:07 -0400

    side menu svgs fill to currentColor

--- packages/shared-client/src/svgs/common/SvgMines.tsx
+++ packages/shared-client/src/svgs/common/SvgMines.tsx
@@ -8,7 +8,7 @@ export const SvgMines = (props: SVGProps<SVGSVGElement>) => (
     <g clipPath="url(#clip0_4000_7656)">
       <path
         d="M14.1 3.3L16 1.4L14.6 0L12.7 1.9L12 1.2L10.4 2.8C7.69995 1.4 4.39995 1.8 2.09995 4C-0.600049 6.7 -0.600049 11.2 2.09995 13.9C4.79995 16.6 9.29995 16.6 12 13.9C14.2 11.7 14.6 8.3 13.2 5.6L14.8 4L14.1 3.3Z"
-        fill="#AFAC9F"
+        fill="currentColor"
       />
     </g>
     <defs>
