47cedaee3fe6d6444c81bc6727469aaf70f6a194
josue <josue@pidwin.com>
2025-04-17 13:24:21 -0400

    add dateranges const to Transactions service

--- packages/shared-core/src/services/transactions/Transactions.ts
+++ packages/shared-core/src/services/transactions/Transactions.ts
@@ -1,6 +1,7 @@
 export * from "./constants/categories";
 export * from "./constants/kinds";
 export * from "./constants/statuses";
+export * from "./constants/dateranges";
 
 export * from "./utils/getCategory";
 export * from "./utils/getName";
