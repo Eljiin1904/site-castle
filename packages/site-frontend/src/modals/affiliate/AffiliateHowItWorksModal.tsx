b91b1f9025e56dde935b12691aaf40b193bdce5c
josue <josue@pidwin.com>
2025-04-25 12:35:06 -0400

    AffilaiteHowItWorks Modal translation

--- packages/site-frontend/src/modals/affiliate/AffiliateHowItWorksModal.tsx
+++ packages/site-frontend/src/modals/affiliate/AffiliateHowItWorksModal.tsx
@@ -7,9 +7,11 @@ import { ModalHeader } from "@client/comps/modal/ModalHeader";
 import { ModalSection } from "@client/comps/modal/ModalSection";
 import { Paragraph } from "@client/comps/paragraph/Paragraph";
 import { Dialogs } from "@client/services/dialogs";
+import { useTranslation } from "@core/services/internationalization/internationalization";
 
 export const AffiliateHowItWorksModal = () => {
   
+  const {t} = useTranslation(["referrals"]);
   const small = useIsMobileLayout();
   return (
     <Modal
@@ -17,36 +19,36 @@ export const AffiliateHowItWorksModal = () => {
       onBackdropClick={() => Dialogs.close("primary")}
     >
       <ModalHeader
-        heading="How do Referrals Work?"
+        heading={t("howItWorks.modal.title")}
         onCloseClick={() => Dialogs.close("primary")}
         headerContent={ <Paragraph mt={small ? 16: 24}>
-          {"Refer a friend to Sandcasino and earn free money from all of their bets!"}
+         {t("howItWorks.modal.description")}
         </Paragraph>}
       />
       <ModalBody pt={0}>
         <Div fx column gap={small ? 20: 32} pt={small ? 24: 32}  borderTop borderColor="brown-4">
           <ModalSection>
           <Heading as="h3" size={small ? 20: 16} fontWeight="regular" textTransform="uppercase">
-            {"Step 1"}
+          {t("howItWorks.modal.list.item1.title")}
           </Heading>
           <Paragraph mt={12}>
-            {"Have a friend that would love Sandcasino? Help them sign up using your Referral Link, or your Code, and they will be counted as your referral!"}
+          {t("howItWorks.modal.list.item1.description")}
           </Paragraph>
           </ModalSection>
           <ModalSection>
           <Heading as="h3"size={small ? 20: 16} fontWeight="regular" textTransform="uppercase">
-            {"Step 2"}
+          {t("howItWorks.modal.list.item2.title")}
           </Heading>
           <Paragraph mt={12}>
-            {"You will earn 10% commission from the house edge on all bets placed by your referrals whether they win or lose."}
+          {t("howItWorks.modal.list.item2.description")}
           </Paragraph>
          </ModalSection>
          <ModalSection>
           <Heading as="h3" size={small ? 20: 16} fontWeight="regular"  textTransform="uppercase">
-            {"Step 3"}
+          {t("howItWorks.modal.list.item3.title")}
           </Heading>
           <Paragraph mt={12}>
-            {"Your referral earnings are immediately available to be claimed!"}
+          {t("howItWorks.modal.list.item3.description")}
           </Paragraph>
          </ModalSection>
          </Div>        
