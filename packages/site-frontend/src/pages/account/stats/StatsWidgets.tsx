fec44701475e419a7851b29cee2ab4177b7332c0
josue <josue@pidwin.com>
2025-04-25 12:35:52 -0400

    User Stats page using StatWidget and WidgetContainer comps

--- packages/site-frontend/src/pages/account/stats/StatsWidgets.tsx
+++ packages/site-frontend/src/pages/account/stats/StatsWidgets.tsx
@@ -1,17 +1,13 @@
 import { useAppSelector } from "#app/hooks/store/useAppSelector";
 import { SvgBets } from "#app/svgs/common/SvgBets";
-import { CardSection } from "@client/comps/cards/CardSection";
 import { Div } from "@client/comps/div/Div";
-import { Heading } from "@client/comps/heading/Heading";
-import { Span } from "@client/comps/span/Span";
-import { Tokens } from "@client/comps/tokens/Tokens";
-import { Vector } from "@client/comps/vector/Vector";
 import { SvgTransaction } from "@client/svgs/common/SvgTransaction";
 import { useTranslation } from "@core/services/internationalization/internationalization";
 import { useMostPopularGame } from "./useMostPopularGame";
 import { SvgDeposit } from "#app/svgs/notifications/SvgDeposit";
 import { SvgWithdraw } from "#app/svgs/notifications/SvgWithdraw";
 import { StatWidget } from "#app/comps/stats/StatWidget";
+import { WidgetContainer } from "#app/comps/stats/WidgetContainer";
 
 export const StatsWidgets = () => {
 
@@ -29,18 +25,18 @@ export const StatsWidgets = () => {
     gap={small ? 16: 24}
     wrap={small}
   >
-    <Div gap={small ? 16: 24} fx column={small}>
-    <StatWidget
-      title={t(`games:${popularGameData?.game}`)}
-      description={t('stats.mostPopularGame')}
-      icon={popularGameData.icon ?? SvgBets} />
+    <WidgetContainer column={small}>
       <StatWidget
-      tokens={stats.wagerAmount}
-      description={t('stats.totalWagered')}
-      icon={SvgTransaction}
+        title={t(`games:${popularGameData?.game}`)}
+        description={t('stats.mostPopularGame')}
+        icon={popularGameData.icon ?? SvgBets} />
+      <StatWidget
+        tokens={stats.wagerAmount}
+        description={t('stats.totalWagered')}
+        icon={SvgTransaction}
       />
-    </Div>
-    <Div gap={small ? 16: 24} fx column={small}>
+    </WidgetContainer>
+    <WidgetContainer column={small}>
       <StatWidget
         tokens={ (stats.depositAmount ?? 0 )}
         description={t('transactions.type.deposits')}
@@ -56,6 +52,6 @@ export const StatsWidgets = () => {
         description={t('stats.pnlShort')}
         icon={SvgBets}
       />
-    </Div>    
+    </WidgetContainer>    
   </Div>)
 };
