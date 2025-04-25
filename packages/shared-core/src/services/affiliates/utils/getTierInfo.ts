f430d0478d192e0e6485cc91f1b07598543ce18e
josue <josue@pidwin.com>
2025-04-25 10:38:55 -0400

    error key for getTierInfo

--- packages/shared-core/src/services/affiliates/utils/getTierInfo.ts
+++ packages/shared-core/src/services/affiliates/utils/getTierInfo.ts
@@ -2,7 +2,7 @@ import { tiers } from "../constants/tiers";
 
 export function getTierInfo(tier: number) {
   if (tier >= tiers.length) {
-    throw new Error("Tier out of range.");
+    throw new Error("validations:errors.tiers.outOfRange");
   }
   return tiers[tier];
 }
