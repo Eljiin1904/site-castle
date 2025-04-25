0226b513c247595e14f384e8514bf1cdc3afe419
josue <josue@pidwin.com>
2025-04-25 12:36:20 -0400

    Affiliate pages back to default from Chicken

--- packages/site-frontend/src/pages/affiliate/referrals/ReferralsBody.tsx
+++ packages/site-frontend/src/pages/affiliate/referrals/ReferralsBody.tsx
@@ -8,17 +8,12 @@ import { ReferralsHeader } from "./ReferralsHeader";
 import { ReferralsTable } from "./ReferralsTable";
 import { ReferralsFooter } from "./ReferralsFooter";
 import { StatGrid } from "./StatGrid";
-import { ReferralHeaderHistory } from "./ReferralHistoryHeader";
-import { PageTitle } from "@client/comps/page/PageTitle";
-import { Heading } from "@client/comps/heading/Heading";
-import { useIsMobileLayout } from "#app/hooks/style/useIsMobileLayout";
 
 export const ReferralsBody = () => {
   const limit = 10;
   const [timeIndex, setTimeIndex] = useState(3);
   const [sortIndex, setSortIndex] = useState(0);
   const [page, setPage] = useState(1);
-  const small = useIsMobileLayout();
 
   const statsQ = useQuery({
     queryKey: ["referral-stats", timeIndex],
@@ -66,13 +61,9 @@ export const ReferralsBody = () => {
     <Div
       fx
       column
-      gap={small ? 24: 40}
+      gap={16}
     >
-      <PageTitle
-        heading="Your Referrals"
-        mt={small ? 32 : 36}
-      />
-      {/* <ReferralsHeader
+      <ReferralsHeader
         timeIndex={timeIndex}
         sortIndex={sortIndex}
         isLoading={statsQ.isLoading || referralsQ.isLoading}
@@ -82,31 +73,21 @@ export const ReferralsBody = () => {
           statsQ.refetch();
           page === 1 ? referralsQ.refetch() : setPage(1);
         }}
-      /> */}
+      />
       <StatGrid stats={stats} />
       <Div
         fx
         column
-        gap={40}
       >
-        <Heading
-          as="h2"
-          size={24}
-          mt={16}
-          fontWeight="regular"
-          textTransform="uppercase"
-        >
-          Referral History
-        </Heading>
         <ReferralsTable
           referrals={referrals}
           isLoading={referralsQ.isLoading}
         />
-        {/* <ReferralsFooter
+        <ReferralsFooter
           page={page}
           hasNext={referrals.length !== 0 && referrals.length % limit === 0}
           setPage={setPage}
-        /> */}
+        />
       </Div>
     </Div>
   );
