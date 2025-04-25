575258e1b0347e04c11c2af04d727389217c722d
josue <josue@pidwin.com>
2025-04-25 10:40:16 -0400

    logo click will redirect to homepage instead of opening/closing menu

--- packages/site-frontend/src/app/menu/panel/MenuHeader.tsx
+++ packages/site-frontend/src/app/menu/panel/MenuHeader.tsx
@@ -3,6 +3,7 @@ import { Vector } from "@client/comps/vector/Vector";
 import { SvgMenu } from "@client/svgs/common/SvgMenu";
 import { SvgSiteLogo } from "@client/svgs/site/SvgSiteLogo";
 import "./MenuHeader.scss";
+import { useNavigate } from "react-router-dom";
 
 export const MenuHeader = ({
   collapsed,
@@ -11,6 +12,7 @@ export const MenuHeader = ({
   collapsed: boolean;
   handleToggle: (x: boolean) => void;
 }) => {
+  const navigate = useNavigate();
   return (
     <Div
       className="MenuHeader"
@@ -20,7 +22,6 @@ export const MenuHeader = ({
     >
       <Div
         hover="highlight"
-        onClick={() => handleToggle(!collapsed)}
         zIndex={15}
       >
         <Vector
@@ -28,12 +29,14 @@ export const MenuHeader = ({
           size={20}
           color="sand"
           mr={16}
+          onClick={() => handleToggle(!collapsed)}
         />
         <Vector
           as={SvgSiteLogo}
           size={20}
           color="sand"
           mr={14}
+          onClick={() => navigate("/")}
         />
       </Div>
     </Div>
