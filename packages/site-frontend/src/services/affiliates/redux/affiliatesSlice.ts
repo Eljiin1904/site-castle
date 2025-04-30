f576452d318ac54f6becb74ea5471822a873be4c
josue <josue@pidwin.com>
2025-04-30 11:34:24 -0400

    reducer for update campaign stats

--- packages/site-frontend/src/services/affiliates/redux/affiliatesSlice.ts
+++ packages/site-frontend/src/services/affiliates/redux/affiliatesSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { UserCampaigns } from "@core/types/users/UserCampaigns";
+import { StreamUpdate } from "@core/types/database/DatabaseStreamEvent";
 
 interface AffiliateState {
 
@@ -36,11 +37,26 @@ export const affiliatesSlice = createSlice({
     insertCampaign: reducer<UserCampaigns>((state, { payload }) => {
       const existingCampaign = state.campaigns.find(campaign => campaign.campaignId === payload.campaignId);
       if (!existingCampaign) {
-        state.campaigns.push(payload);
+        state.campaigns = [payload, ...state.campaigns];
         state.referredFriends += payload.referralCount ?? 0;
         state.unclaimedCommission += payload.commissionBalance ?? 0; 
         state.totalComission += payload.commissionTotal ?? 0;
         state.totalFriendsWagered += payload.wagerAmount ?? 0;
+      }
+    }),
+    updateCampaign: reducer<StreamUpdate>((state, { payload }) => {
+      const existingCampaign = state.campaigns.find(campaign => campaign._id === payload.documentId);
+      if (existingCampaign) {
+        const updatedCampaign = { ...existingCampaign, ...payload.updatedFields };
+        
+        state.referredFriends += (updatedCampaign.referralCount ?? 0) - (existingCampaign.referralCount ?? 0);
+        state.unclaimedCommission += (updatedCampaign.commissionBalance ?? 0) - (existingCampaign.commissionBalance ?? 0);
+        state.totalComission += (updatedCampaign.commissionTotal ?? 0) - (existingCampaign.commissionTotal ?? 0);
+        state.totalFriendsWagered += (updatedCampaign.wagerAmount ?? 0) - (existingCampaign.wagerAmount ?? 0);
+        
+        state.campaigns = state.campaigns.map(campaign =>
+          campaign._id === payload.documentId ? updatedCampaign : campaign
+        );
       }
     }),
     setSelectedCampaignId: reducer<string | null>((state, { payload }) => {
@@ -55,6 +71,7 @@ export const affiliatesSlice = createSlice({
 export const {
   setCampaigns,
   insertCampaign,
+  updateCampaign,
   setSelectedCampaignId,
   setLimit,
 } = affiliatesSlice.actions;
