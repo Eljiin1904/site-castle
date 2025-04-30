f613d4ff4e378514c95507693c1d96584bd48ee7
josue <josue@pidwin.com>
2025-04-30 11:43:07 -0400

    campaign relate updates

--- packages/shared-backend/src/listeners/campaign/helpers/ticketStream.ts
+++
@@ -1,6 +0,0 @@
-import { Database } from "@server/services/database";
-
-export const ticketStream = Database.createStream({
-  collection: "user-campaigns",
-  maxLogSize: 0,
-});
--- packages/shared-backend/src/routes/affiliates/getCampaigns.ts
+++ packages/shared-backend/src/routes/affiliates/getCampaigns.ts
@@ -1,25 +1,20 @@
-import { subDays } from "date-fns";
+
 import { Validation } from "@core/services/validation";
 import { HandledError } from "@server/services/errors";
-
-import { Affiliates } from "@server/services/affiliates";
 import { Http } from "#app/services/http";
 import { Database } from "@server/services/database";
 
+// NOT IN USE (TO REMOVE)
 export default Http.createApiRoute({
   type: "post",
   path: "/get-campaigns",
   secure: true,
   body: Validation.object({
-    // timeIndex: Validation.index("Time index", 3),
-    // sortIndex: Validation.index("Sort index", 4),
     limit: Validation.limit(),
     page: Validation.page(),
   }),
   callback: async (req, res) => {
     const {
-      // timeIndex,
-      // sortIndex,
       limit,
       page,
     } = req.body;
@@ -34,35 +29,5 @@ export default Http.createApiRoute({
       .toArray();
 
     res.json({ campaigns: campaigns });
-    // return campaigns;
-
-    //  const minDate = [
-    //    subDays(Date.now(), 1),
-    //    subDays(Date.now(), 7),
-    //    subDays(Date.now(), 30),
-    //    new Date(0),
-    //  ][timeIndex];
-
-    // const maxDate = new Date();
-
-    //  const referrals = await Affiliates.aggregateReports({
-    //    affiliateId: user._id,
-    //    minDate,
-    //    maxDate,
-    //    sort: {
-    //      ...[
-    //        { commissionAmount: -1 },
-    //        { depositAmount: -1 },
-    //        { wagerAmount: -1 },
-    //        { lastDepositDate: -1 },
-    //        { referDate: -1 },
-    //      ][sortIndex],
-    //      _id: 1,
-    //    },
-    //    limit,
-    //    page,
-    //  });
-
-    //  res.json({ referrals });
   },
 });
