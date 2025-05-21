cb5d03b382ea0263b900e205374bd89c98da4d4f
civilizador <asim@pidwin.com>
2025-05-21 15:06:22 -0400

    testing end to end solution for vite

--- packages/shared-server/src/config.ts
+++ packages/shared-server/src/config.ts
@@ -74,6 +74,7 @@ export async function loadSecrets(overrides: Record<string, string> = {}) {
     console.log("fetching secrets");
     let env = config.env as string;
     if (config.env === "devcloud" || config.env === "staging") {
+      console.log("The corrent environment is being used is : ", config.env);
       env = "development";
     }
 
