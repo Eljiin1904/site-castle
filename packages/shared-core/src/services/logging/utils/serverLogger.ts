b4bd28bf8d63b12ffc48766f3c18c7314240bb48
giovanni-pidwin <giovanni@pidwin.com>
2025-05-21 17:17:08 -0400

    Remove SDK for now

--- packages/shared-core/src/services/logging/utils/serverLogger.ts
+++ packages/shared-core/src/services/logging/utils/serverLogger.ts
@@ -1,18 +1,18 @@
 import { LoggerConfig } from "#core/types/logging/LoggerConfig";
-import AWS from "aws-sdk";
+// import AWS from "aws-sdk";
 import winston from "winston";
-import CloudWatchTransport from "winston-cloudwatch";
+// import CloudWatchTransport from "winston-cloudwatch";
 
 const IS_DEV = process.env.NODE_ENV === "devcloud";
 const IS_TEST = process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development";
 
-if (!IS_DEV) {
-  AWS.config.update({
-    region: process.env.AWS_REGION || "us-east-1",
-    accessKeyId: process.env.AWS_ID,
-    secretAccessKey: process.env.AWS_SECRET,
-  });
-}
+// if (!IS_DEV) {
+//   AWS.config.update({
+//     region: process.env.AWS_REGION || "us-east-1",
+//     accessKeyId: process.env.AWS_ID,
+//     secretAccessKey: process.env.AWS_SECRET,
+//   });
+// }
 
 const { align, colorize, combine, errors, timestamp, json, simple, printf } = winston.format;
 
@@ -46,22 +46,23 @@ const createLogger = (options: LoggerConfig) => {
             json(),
           ),
     transports:
-      IS_DEV || IS_TEST
-        ? [
-            new winston.transports.Console({
-              format: combine(colorize(), simple()),
-            }),
-          ]
-        : [
-            new CloudWatchTransport({
-              logGroupName: options.logGroupName,
-              logStreamName: options.logStreamName,
-              awsRegion: process.env.AWS_REGION || "us-east-1",
-              jsonMessage: true,
-              retentionInDays: 30,
-              uploadRate: 30000, // how often logs have to be sent to AWS, default 2000ms
-            }),
-          ],
+      // IS_DEV || IS_TEST
+      // ?
+      [
+        new winston.transports.Console({
+          format: combine(colorize(), simple()),
+        }),
+      ],
+    // : [
+    //     new CloudWatchTransport({
+    //       logGroupName: options.logGroupName,
+    //       logStreamName: options.logStreamName,
+    //       awsRegion: process.env.AWS_REGION || "us-east-1",
+    //       jsonMessage: true,
+    //       retentionInDays: 30,
+    //       uploadRate: 30000, // how often logs have to be sent to AWS, default 2000ms
+    //     }),
+    //   ],
   });
 
   return {
