139e3fd2cdaeddef1abe5271969782e95298b48b
josue <josue@pidwin.com>
2025-06-06 18:53:26 -0400

    crash comp stories

--- packages/site-frontend/src/stories/site-frontend/crash/CrashEvent.stories.ts
+++ packages/site-frontend/src/stories/site-frontend/crash/CrashEvent.stories.ts
@@ -48,13 +48,16 @@ const meta = {
       description: "If true, the event starts at the line. Defaults to false."
     },
     animated: {
-      control: {
-        type: "boolean"
-      },
-      description: "If true, the event will animate to its position. Defaults to true."
+      table: {
+        disable: true
+      }
+    },
+    initialHeight: {
+      table: {
+        disable: true
+      }
     }
   }
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
 } satisfies Meta<typeof CrashEvent>;
 
 
@@ -65,12 +68,6 @@ type Story = StoryObj<typeof CrashEvent>;
  * Crash events are used to visualize the crash multiplier in the game, and can be used to indicate when a player has crashed out of the game.
  * A green event indicates an active multiplier, while a red event indicates a crash. Red event is display next to a green one when a crash occurs.
  * At the start of each round, an animation is done using randomly generated events to simulate the stock market.<br/><br/>
- * @param height - The height of the event in pixels. Minimum is 4.<br/>
- * @param color - Color of the event. Only green and red. Defaults to green.<br/>
- * @param width - Width of the event in pixels. Defaults to 10.<br/>
- * @param position - The position of the event in the chart. Defaults to 0.<br/>
- * @param startAtLine - If true, the event starts at the line. Defaults to false.<br/>
- * @param animated - If true, the event will animate to its position. Defaults to true.<br/>
  */
 export const CrashContentGame: Story = {
   args: {
--- packages/site-frontend/src/stories/site-frontend/crash/CrashMultiplierLine.stories.ts
+++ packages/site-frontend/src/stories/site-frontend/crash/CrashMultiplierLine.stories.ts
@@ -7,15 +7,48 @@ const meta = {
   component: CrashMultiplierLine,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
+  argTypes: {
+    position: {
+      control: {
+        type: "number",
+        min: 0,
+        max: 1000,
+        step: 1
+      },
+      description: "Position of the multiplier line in the chart. Defaults to 0."
+    },
+    status: {
+      options: ["waiting", "pending", "simulating", "completed"],
+      mapping: {
+        waiting: "waiting",
+        pending: "pending",
+        simulating: "simulating",
+        completed: "completed"
+      },
+      description: "Status of the multiplier line. Defaults to waiting.",
+    },
+    cashout: {
+      control: {
+        type: "number"
+      },
+      description: "If cashout is > 1, multiplier line is green and dashed and display the cashout value. Defaults to 1.",
+    }
+  }
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
 } satisfies Meta<typeof CrashMultiplierLine>;
 
 export default meta;
 type Story = StoryObj<typeof CrashMultiplierLine>;
-
+/**
+ * A component that represents the state of the current multiplier in the Crash game.
+ * It goes up while the game is running, and stops at the current multiplier when the game crash.
+ * Initially, it starts at 1x and color is gray until the game starts.
+ * Once the game starts, it changes to green and starts increasing.
+ * When the game crashes, If the user cashes out before the crash, the multiplier is displayed in green.
+ * If the user does not cash out before the crash, the multiplier is displayed in red. <br/>
+ */
 export const CrashContentGame: Story = {
   args: {
-    // color: "bright-green",
     position: 0,
   }
 };
