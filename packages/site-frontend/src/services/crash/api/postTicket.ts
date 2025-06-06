20736440912c116d75d43fe0693235e7fdd40320
josue <josue@pidwin.com>
2025-06-06 18:55:32 -0400

    add documentation to crash client service

--- packages/site-frontend/src/services/crash/api/postTicket.ts
+++ packages/site-frontend/src/services/crash/api/postTicket.ts
@@ -1,6 +1,17 @@
 import { Http } from "@client/services/http";
 import { CrashTicketDocument } from "@core/types/crash/CrashTicketDocument";
 
+/**
+ * Sends a request to create a crash ticket for the current or next round.
+ *
+ * @param data - The ticket data to be posted.
+ * @param data.roundId - The unique identifier for the round.
+ * @param data.betAmount - The amount of the bet placed.
+ * @param data.betToken - (Optional) The token used for the bet.
+ * @param data.targetMultiplier - (Optional) The target multiplier for the bet.
+ * @param data.nextRound - (Optional) Indicates whether the ticket is for the next round.
+ * @returns A promise that resolves to an object containing the results of the posted ticket.
+ */
 export function postTicket(data: {
   roundId: string;
   betAmount: number;
