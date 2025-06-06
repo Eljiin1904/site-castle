165d5935f3d896133dff2ba95cedfa20f6f762c1
josue <josue@pidwin.com>
2025-06-06 18:51:58 -0400

    add documentation to crash core

--- packages/shared-core/src/services/crash/constants/roundTimes.ts
+++ packages/shared-core/src/services/crash/constants/roundTimes.ts
@@ -1,6 +1,11 @@
+/**
+ * All the round times for the crash game.
+ * These times are used to control the flow of the game and the user interface.
+ */
 export const roundTimes = {
-  waiting: 4000,
-  pending: 100,
-  completed: 4000,
-  delay: 1000,
+  waiting: 4000, // Time before the round starts, allow users to place bets
+  pending: 100, // Time before the round starts, allow users to place bets
+  completed: 4000, // Time after the round ends, process bets and show results
+  delay: 1000, // Delay time between server and client, used for latency compensation
+  intervalFrequency: 150, // Frequency of the interval that updates the round time in the client, and processes auto cashouts
 };
--- packages/shared-core/src/services/crash/utils/getMultiplierForTime.ts
+++ packages/shared-core/src/services/crash/utils/getMultiplierForTime.ts
@@ -1,3 +1,13 @@
+/**
+ * Calculates a multiplier based on the provided timer value.
+ * 
+ * The multiplier is computed using a formula that involves exponential growth
+ * and is rounded down to two decimal places.
+ * 
+ * @param timer - The timer value in milliseconds. This value is used to calculate
+ *                the multiplier based on a predefined formula.
+ * @returns The calculated multiplier as a number rounded to two decimal places.
+ */
 export function getMultiplierForTime(timer: number): number {
 
   return Math.floor(1.0024 * Math.pow(1.0718, timer / 1000)* 100) / 100;
