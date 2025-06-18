c5365f1c4f7e5d0a746776becf4f1c2d359cea80
josue <josue@pidwin.com>
2025-06-18 12:38:37 -0400

    PageNotice skinning

--- packages/shared-client/src/comps/page/PageNotice.tsx
+++ packages/shared-client/src/comps/page/PageNotice.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import classNames from "classnames";
 import { Button } from "../button/Button";
 import { Div } from "../div/Div";
@@ -14,73 +14,150 @@ export type PageNoticeProps = Omit<StyledLayoutProps, "children"> & {
   icon?: Svg;
   title: string;
   titleSize?: Unit;
-  message: string | JSX.Element;
+  message?: string | JSX.Element;
   description?: string;
+  small?: boolean;
   buttonLabel?: string;
   onButtonClick?: () => void;
+  children?: JSX.Element | JSX.Element[];
 };
 
 export const PageNotice: FC<PageNoticeProps> = ({
   className,
-  image,
-  icon,
-  title,
-  titleSize = 24,
-  message,
-  description,
-  buttonLabel,
-  onButtonClick,
+  small,
   ...forwardProps
 }) => {
   return (
     <Div
       className={classNames("PageNotice", className)}
-      column
       center
       grow
       gap={40}
+      column={small}
+      pb={small? 40: 0}
       {...forwardProps}
     >
-      {image && <Img  type="png" path={image} width="256px" />}
-      {icon && <Vector as={icon} size={40} />}
-      
-      <Heading
-        as="h1"
-        className="title"
-        size={titleSize}
-        textAlign="center"
-        textTransform="uppercase"
-      >
-        {title}
-      </Heading>
-      <Div
-        display="block"
-        className="message"
-        fontSize={14}
-        textAlign="center"
-        px={16}
-      >
-        {message}
-      </Div>
-      {buttonLabel && (
-        <Button
-          kind="primary-yellow"
-          size="md"
-          label={buttonLabel}
-          fx
-          style={{ maxWidth: "400px" }}
-          onClick={onButtonClick}
-        ></Button>
-      )}
-      {description && (
-        <Span
-          fontSize={14}
-          color="red"
-          mt={24}
-        >
-          {description}
-        </Span>
-      )}
+      {small ? (
+          <PageNoticeContentMobile {...forwardProps} />
+        ) : (
+          <PageNoticeContentDesktop {...forwardProps} />
+        )}
     </Div>
   );
 };
+
+const PageNoticeContentMobile: FC<PageNoticeProps> = ({
+  image,
+  icon,
+  title,
+  message,
+  description,
+  buttonLabel,
+  onButtonClick,
+  children
+}) => { 
+  return (
+    <Fragment>
+      <Div fx>
+        {image && <Img type="png" path={image} width="100%" />}
+        {icon && <Vector as={icon} size={40} />}
+      </Div>
+      <Div column fx gap={24} center>
+        <Heading
+          as="h1"
+          className="title"
+          textTransform="uppercase"
+          size={32}
+          lineHeight={32}
+          textAlign="center"
+        >
+          {title}
+        </Heading>
+        {message && (
+          <Span
+            className="message"
+            textAlign="center"
+          >
+            {message}
+          </Span>
+        )}
+        {buttonLabel && (
+          <Button
+            kind="primary-yellow"
+            size="md"
+            label={buttonLabel}
+            onClick={onButtonClick}
+            style={{ width: "fit-content" }}
+          ></Button>
+        )}
+        {description && (
+          <Span
+            color="double-red"
+            textAlign="center"
+          >
+            {description}
+          </Span>
+        )}
+      </Div>      
+    </Fragment>
+  );
+};
+const PageNoticeContentDesktop: FC<PageNoticeProps> = ({
+  image,
+  icon,
+  title,
+  titleSize = 64,
+  message,
+  description,
+  buttonLabel,
+  onButtonClick,
+  children
+}) => {
+  return (
+    <Fragment>
+      <Div column width={400} gap={56}>
+        <Heading
+          as="h1"
+          className="title"
+          size={titleSize}
+          textTransform="uppercase"
+          lineHeight={56}
+        >
+          {title}
+        </Heading>
+        {message && (
+          <Div
+          display="block"
+          className="message"
+          fontSize={14}
+        >
+          {message}
+        </Div>
+        )}
+        {buttonLabel && (
+          <Button
+            kind="primary-yellow"
+            size="md"
+            label={buttonLabel}
+            onClick={onButtonClick}
+            style={{ width: "fit-content" }}
+          ></Button>
+        )}
+        {description && (
+          <Span
+            fontSize={14}
+            color="double-red"
+          >
+            {description}
+          </Span>
+        )}
+      </Div>
+      <Div width={400}>
+        {image && <Div>
+          <Img  type="png" path={image} width="100%" style={{maxWidth: '451px'}} />
+          {children}
+        </Div>}
+        {icon && <Vector as={icon} size={40} />}
+      </Div>
+    </Fragment>)
+}
