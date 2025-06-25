f6159dec71eb75ee91078901486029d67340cc39
josue <josue@pidwin.com>
2025-06-25 16:50:30 -0400

    KYC skinning and trans

--- packages/site-frontend/src/pages/about/kyc/KycBody.tsx
+++ packages/site-frontend/src/pages/about/kyc/KycBody.tsx
@@ -1,86 +1,80 @@
-export const KycBody = () => (
-  <div>
-    <h1>KYC Policy</h1>
-    <p>Version 1.0.0 - Last updated: 19 July 2024</p>
+import { LegalSection } from "#app/comps/legal/LegalSection";
+import { useIsMobileLayout } from "#app/hooks/style/useIsMobileLayout";
+import { Div } from "@client/comps/div/Div";
+import { OrderedList } from "@client/comps/list/OrderedList";
+import { PageTitle } from "@client/comps/page/PageTitle";
+import { Paragraph } from "@client/comps/paragraph/Paragraph";
+import { useTranslation } from "@core/services/internationalization/internationalization";
+import { Fragment } from "react";
 
-    <section>
-      <p>
-        The Company adheres to and complies with "Know your customer"
-        principles, which aim to prevent financial crime and money laundering
-        through client identification and due diligence.
-      </p>
-      <p>
-        The Company reserves the right, at any time, to ask for any KYC
-        documentation it deems necessary to determine the identity and location
-        of a user. We reserve the right to restrict the service, payment, or
-        withdrawal until identity is sufficiently determined, or for any other
-        reason in our sole discretion.
-      </p>
-      <p>
-        We take a risk-based approach and perform strict due diligence checks
-        and ongoing monitoring of all clients, customers, and transactions. We
-        use a multi-step process of due diligence checks, depending on the risk,
-        transaction, and customer type.
-      </p>
-      <p>
-        All customers must verify their email address to open an account. In
-        order to deposit, all customers must provide their name, address,
-        occupation and date of birth. If customer activity alerts any of our
-        monitoring parameters, the customer must complete the activities
-        required by our identity verification service. Documents that the
-        identity verification service may require include, but are not limited
-        to: government issued ID, selfies to be scanned by AI. We may require
-        additional documentation for manual review, such as: proof of address,
-        which includes but is not limited to, a utility bill; proof of funds,
-        which includes but is not limited to, a bank statement or a paycheck.
-      </p>
-    </section>
+export const KycBody = () => {
 
-    <section>
-      <h2>Guideline for the "KYC Process"</h2>
-      <ol>
-        <li>Proof of ID</li>
-        <ol type="a">
-          <li>Signature is present.</li>
-          <li>
-            Country is not one of the Restricted Countries, see Terms and
-            Conditions for the list of countries.
-          </li>
-        </ol>
-        <li>Proof of Residence</li>
-        <ol type="a">
-          <li>Bank Statement or Utility Bill</li>
-          <li>
-            Country is not one of the Restricted Countries, see Terms and
-            Conditions for the list of countries.
-          </li>
-          <li>
-            Full Name matches client's name and is same as in proof of ID.
-          </li>
-          <li>Date of Issue is within the last 3 months.</li>
-        </ol>
-        <li>Selfie with ID</li>
-        <ol type="a">
-          <li>Holder is the same as in the ID document.</li>
-          <li>
-            ID document is consistently used; photo and ID number are the same.
-          </li>
-        </ol>
-      </ol>
-    </section>
+ const { t } = useTranslation(["legal//kyc"]);
+ const small = useIsMobileLayout();
+  return (
+    <Fragment>
+      <Div
+        column
+        gap={small ? 16 : 24}
+      >
+        <PageTitle heading={t('title')}/>       
+        <Paragraph>{t('version',{value: {date: '16 January 2025'}})}</Paragraph>
+      </Div>
+      <IntroSection/>
+      <GuidelineSection/>
+      <NotesSection/>
+    </Fragment>
+  );
+};
 
-    <section>
-      <h2>Notes on the "KYC Process"</h2>
-      <ol>
-        <li>
-          When the KYC process is unsuccessful, the reason is documented and
-          escalated.
-        </li>
-        <li>
-          Once all proper documents are provided and deemed satisfactory, the
-          account will be approved.
-        </li>
-      </ol>
-    </section>
-  </div>
-);
+const IntroSection = () => {
+  const { t } = useTranslation(["legal//kyc"]);
+  return (<LegalSection>
+    <Paragraph>{t('paragraph1')}</Paragraph>
+    <Paragraph>{t('paragraph2')}</Paragraph>
+    <Paragraph>{t('paragraph3')}</Paragraph>
+  </LegalSection>);
+};
+const GuidelineSection = () => {
+  const { t } = useTranslation(["legal//kyc"]);
+  return (<LegalSection heading={t('guideline.heading')}>
+   <Div fx column gap={8}>
+    <OrderedList items={[t('guideline.item1')]}/>
+    <OrderedList pl={80} items={[
+      t('guideline.list1.item1'),
+      t('guideline.list1.item2'),
+    ]}
+    type="a"
+    />
+   </Div>
+   <Div fx column gap={8}>
+    <OrderedList items={[t('guideline.item2')]} start={2}/>
+    <OrderedList pl={80} items={[
+      t('guideline.list2.item1'),
+      t('guideline.list2.item2'),
+      t('guideline.list2.item3')
+    ]}
+    type="a"
+    />
+   </Div>
+   <Div fx column gap={8}>
+    <OrderedList items={[t('guideline.item3')]} start={3}/>
+    <OrderedList pl={80} items={[
+      t('guideline.list3.item1'),
+      t('guideline.list3.item2'),
+    ]}
+    type="a"
+    />
+   </Div>
+  </LegalSection>);
+};
+const NotesSection = () => {
+  const { t } = useTranslation(["legal//kyc"]);
+  return (<LegalSection heading={t('notes.heading')}>
+    <OrderedList items={[
+      t('notes.note1'),
+      t('notes.note2'),
+    ]}
+    />
+  </LegalSection>);
+};
