f8fdbf7846f84ec22b01cde1a8e0053cdc8a2dea
josue <josue@pidwin.com>
2025-06-25 15:41:14 -0400

    Legal Section and header components

---
+++ packages/site-frontend/src/comps/legal/LegalSection.tsx
@@ -0,0 +1,14 @@
+import { useIsMobileLayout } from "#app/hooks/style/useIsMobileLayout";
+import { Div } from "@client/comps/div/Div";
+import { LegalSubHeader } from "./LegalSubHeader";
+
+export const LegalSection = ({heading, children}: {heading: string, children: React.ReactNode}) => {
+  const small = useIsMobileLayout();
+  return (
+    <Div  column
+    gap={small ? 16 : 24}>
+      <LegalSubHeader text={heading} />
+      {children}
+    </Div>
+  );
+};
