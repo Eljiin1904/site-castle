e9c2f42bcb46d3f2781644f86633cdfd757391d9
josue <josue@pidwin.com>
2025-06-25 16:50:16 -0400

    header optional LegalSection comp

--- packages/site-frontend/src/comps/legal/LegalSection.tsx
+++ packages/site-frontend/src/comps/legal/LegalSection.tsx
@@ -2,12 +2,12 @@ import { useIsMobileLayout } from "#app/hooks/style/useIsMobileLayout";
 import { Div } from "@client/comps/div/Div";
 import { LegalSubHeader } from "./LegalSubHeader";
 
-export const LegalSection = ({heading, children}: {heading: string, children: React.ReactNode}) => {
+export const LegalSection = ({heading, children}: {heading?: string, children: React.ReactNode}) => {
   const small = useIsMobileLayout();
   return (
     <Div  column
     gap={small ? 16 : 24}>
-      <LegalSubHeader text={heading} />
+      {heading && <LegalSubHeader text={heading} />}
       {children}
     </Div>
   );
