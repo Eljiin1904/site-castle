7645a1e40745a4aaeb91f42e24fc51290c162add
josue <josue@pidwin.com>
2025-07-01 12:24:38 -0400

    update getRequestError to manage validations too

--- packages/shared-client/src/services/http/utils/getRequestError.ts
+++ packages/shared-client/src/services/http/utils/getRequestError.ts
@@ -1,9 +1,21 @@
 import { isAxiosError } from "axios";
 
 export function getRequestError(err: unknown) {
-  if (isAxiosError(err) && err.response?.data?.error) {
-    const error = new Error(err.response.data.error as string);
-    error.cause = err.response.data.cause;
+  if (isAxiosError(err)) {
+    const error = new Error();
+    console.error("Error in POST request:", err);
+    if(err.response?.data?.error) {
+      
+      const axiosErrorContent = err.response.data.error;
+      if (typeof axiosErrorContent === "string") {
+        error.message = axiosErrorContent;
+        error.cause = err.response.data.cause;   
+      }
+      else {
+        error.message = axiosErrorContent.key || "An error occurred";
+        error.cause = axiosErrorContent.value || {};
+      }
+    }
     return error;
   } else {
     return err;
