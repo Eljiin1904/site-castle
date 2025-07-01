a5129b6835c5ffbabc948849ea2d3a5bba6b839b
josue <josue@pidwin.com>
2025-07-01 18:09:25 -0400

    no games found message

--- packages/site-frontend/src/pages/games/GamesGrid.tsx
+++ packages/site-frontend/src/pages/games/GamesGrid.tsx
@@ -1,6 +1,8 @@
 import { useAppSelector } from "#app/hooks/store/useAppSelector";
 import { Div } from "@client/comps/div/Div";
 import { HubEightGameDocument } from "@core/types/hub-eight/HubEightGameDocument";
+import { useTranslation } from "@core/services/internationalization/internationalization";
+import { Span } from "@client/comps/span/Span";
 import { GameHubEight } from "./GameHubEight";
 
 export const GamesGrid = ({games}: {
@@ -8,12 +10,16 @@ export const GamesGrid = ({games}: {
 }) => {
 
   const layout = useAppSelector((x) => x.style.mainLayout);
+  const {t} = useTranslation(["games"]);
   return (<Div
       className="SearchResults"
       gap={layout === 'mobile' ? 20 : 24}
       fx
     >
-      {games.map((x, i) => <GameHubEight key={`${x._id}-${i}`}
+      {games.length === 0 ? <Div fx justifyContent="center" alignItems="center" style={{width: '100%'}}>
+        <Span>{t('notGames')}</Span>
+      </Div> :
+      games.map((x, i) => <GameHubEight key={`${x._id}-${i}`}
       image={x.url_background ?? undefined}
       heading={x.name} 
       to={`/games/${x.game_code}`}
