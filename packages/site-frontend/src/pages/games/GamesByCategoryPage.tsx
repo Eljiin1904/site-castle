692c2aa23ecf64e4fea35a9652bbde628b534a31
josue <josue@pidwin.com>
2025-07-03 14:55:43 -0400

    Hub88 games page

--- packages/site-frontend/src/pages/games/GamesByCategoryPage.tsx
+++ packages/site-frontend/src/pages/games/GamesByCategoryPage.tsx
@@ -11,7 +11,6 @@ import { useQuery } from "@tanstack/react-query";
 import { HubEight } from "#app/services/hubEight";
 import { ExternalGameCategory } from "@core/types/hub-eight/GameInformation";
 import { GamesGrid } from "./GamesGrid";
-import { GameSortType } from "@core/services/game/Game";
 import { TableFooterPagination } from "#app/comps/pagination/TableFooterPagination";
 import { useAppDispatch } from "#app/hooks/store/useAppDispatch";
 import { Site } from "#app/services/site";
@@ -21,44 +20,9 @@ export const GamesByCategoryPage = ({category}: {
   category: ExternalGameCategory;
 }) => {
   
-  const dispatch = useAppDispatch();
-  const [sortBy, setSortBy] = useState<GameSortType>("featured");
-  const [selectedProviders, setSelectedProviders] = useState<string[]>([]); 
-  const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(40); 
   const small = useIsMobileLayout();
   const {t} = useTranslation(['games']);
-
-  useEffect(() => {
-    // Reset sort and providers when category changes
-    setSortBy("featured");
-    setSelectedProviders([]);
-    dispatch(Site.setSearch(''));
-  }, [category]);
-
-  let sortIndex = 0;
-  switch (sortBy) {
-    case "featured":
-      sortIndex = 0;
-      break;
-    case "popular":
-      sortIndex = 1;
-      break;
-    case "az":
-      sortIndex = 2;
-      break;
-    default:
-      sortIndex = 3;
-      break;
-  }
-  const query = useQuery({
-    queryKey: ["games-results",category, sortBy, selectedProviders, page],
-    queryFn: () => HubEight.getGameList({ category, products: selectedProviders, page,limit, sortIndex}),
-    placeholderData: (prev) => prev,
-  });
-
-  const games = query.data?.games || [];
-  const total = query.data?.total || 0;
+ 
   return (<Fragment>
     <PageBanner image={`/graphics/${category}-games-tile`} heading={t(`games:${category}`)} description="" content={<></>}/> 
     <SitePage
@@ -66,23 +30,55 @@ export const GamesByCategoryPage = ({category}: {
       gap={small ? 32: 56}
       pb={small ? 32: 56}
     >
-    <Div fx justifyContent="space-between" alignItems="center" gap={small ? 16: 24} column={small}>
-      <GameSearch />
-      <GameProvider selectedProviders={selectedProviders} setSelectedProviders={setSelectedProviders} category={category} />
-      <GameSort sortBy={sortBy} setSortBy={setSortBy} />
-    </Div>
-    
-    <GamesGrid games={games}/>
-    <TableFooterPagination 
-      page={page} 
-      total={total} 
-      limit={limit} 
-      inPage={games.length}
-      setPage={setPage}
-      hasNext={games.length !== 0 && games.length % limit === 0}
-      label={t("game",{count: query.data?.total || 0})}
-      />
+      <GamesByCategory category={category} />
     </SitePage>
   </Fragment>
   );
 };
+
+export const GamesByCategory = ({category}: {category: ExternalGameCategory | undefined}) => {
+  
+  const [sortIndex, setSortIndex] = useState<number>(0);
+  const [selectedProviders, setSelectedProviders] = useState<string[]>([]);
+  const {t} = useTranslation(['games']); 
+  const [page, setPage] = useState(1);
+  const dispatch = useAppDispatch();
+  const small = useIsMobileLayout();
+  const limit = 40;
+
+  const query = useQuery({
+    queryKey: ["games-results",category, sortIndex, selectedProviders, page],
+    queryFn: () => HubEight.getGameList({ category, products: selectedProviders, page,limit, sortIndex}),
+    placeholderData: (prev) => prev,
+  });
+
+  useEffect(() => {
+    // Reset sort and providers when category changes
+    setSortIndex(0);
+    setSelectedProviders([]);
+    dispatch(Site.setSearch(''));
+  }, [category]);
+
+  const games = query.data?.games || [];
+  const total = query.data?.total || 0;
+
+  return (<Fragment>
+      <Div fx justifyContent="space-between" alignItems="center" gap={small ? 16: 24} column={small}>
+        <GameSearch />
+        <GameProvider selectedProviders={selectedProviders} setSelectedProviders={setSelectedProviders} category={category} />
+        <GameSort sortBy={sortIndex} setSortBy={setSortIndex} />
+      </Div>
+      
+      <GamesGrid games={games}/>
+      <TableFooterPagination 
+        page={page} 
+        total={total} 
+        limit={limit} 
+        inPage={games.length}
+        setPage={setPage}
+        hasNext={games.length !== 0 && games.length % limit === 0}
+        label={t("game",{count: query.data?.total || 0})}
+        />
+  </Fragment>
+  );
+}
---
+++ packages/site-frontend/src/pages/games/HubEightGames.tsx
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+import { SitePage } from "#app/comps/site-page/SitePage";
+import { useIsMobileLayout } from "#app/hooks/style/useIsMobileLayout";
+import { useAppSelector } from "#app/hooks/store/useAppSelector";
+import { useTranslation } from "@core/services/internationalization/internationalization";
+import { FeaturedGames } from "#app/comps/games/FeaturedGames";
+import { OriginalGames } from "#app/comps/games/OriginalGames";
+import { PageBanner } from "#app/comps/site-page/PageBanner";
+import { Div } from "@client/comps/div/Div";
+import { GameSearch } from "#app/comps/games/GamesSearch";
+import { GameProvider } from "#app/comps/games/GameProvider";
+import { GameSort } from "#app/comps/games/GameSort";
+import { GamesByCategory } from "./GamesByCategoryPage";
+
+
+export const HubEightGames = () => {
+  
+  const games = useAppSelector((x) => x.site.games) || [];
+  const small = useIsMobileLayout();
+  const {t} = useTranslation(['games']);
+  const featuredGames = games?.filter((x) => x.featured);
+  
+  return (<Fragment>
+    <PageBanner image="/graphics/original-games-tile" heading={t('game',{count: 2})} description="" content={<></>}/> 
+    <SitePage
+      className="GamesPage"
+      gap={small ? 32: 56}
+      pb={small ? 32: 56}
+    >
+      <FeaturedGames items={featuredGames} showSection={true} />
+      <GamesByCategory category={undefined} />
+    </SitePage>
+  </Fragment>
+  );
+};
