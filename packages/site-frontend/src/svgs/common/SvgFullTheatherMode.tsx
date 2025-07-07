05149c59972a20d489dd0bba09068d1f7bb3a3a9
josue <josue@pidwin.com>
2025-07-07 16:30:41 -0400

    svg for fullscreen and theatre mode hub 88 games

---
+++ packages/site-frontend/src/svgs/common/SvgFullTheatherMode.tsx
@@ -0,0 +1,15 @@
+import { SVGProps } from "react";
+
+export const SvgFullTheatherMode = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 16 12"
+    {...props}
+  >
+    <rect x="1" y="1" width="14" height="10" stroke="currentColor" stroke-width="2"/>
+  </svg>
+);
+
+{/* <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="1" y="1" width="14" height="10" stroke="#AFAC9F" stroke-width="2"/>
+</svg> */}
+
