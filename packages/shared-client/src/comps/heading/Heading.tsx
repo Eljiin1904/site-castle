1cd6fd6dd157c27838eda03d075f9abdad79dfe3
josue <josue@pidwin.com>
2025-07-14 11:42:11 -0400

    Heading font size and weight as prop instead of default css

--- packages/shared-client/src/comps/heading/Heading.tsx
+++ packages/shared-client/src/comps/heading/Heading.tsx
@@ -6,6 +6,7 @@ import "./Heading.scss";
 export type HeadingProps = Omit<StyledProps, "as"> & {
   as?: "h1" | "h2" | "h3" | "h4" | "h5";
   family?: StyledProps["fontFamily"];
+  fontWeight?: StyledProps["fontWeight"];
   size?: Unit;
   children: any;
 };
@@ -13,10 +14,11 @@ export type HeadingProps = Omit<StyledProps, "as"> & {
 export const Heading: FC<HeadingProps> = ({
   as = "h3",
   className,
-  size,
+  size = 16,
   fontSize = size,
   family,
   fontFamily = family,
+  fontWeight = "bold",
   children,
   ...forwardProps
 }) => {
@@ -26,6 +28,7 @@ export const Heading: FC<HeadingProps> = ({
       className={classNames("Heading", className)}
       fontSize={fontSize}
       fontFamily={fontFamily}
+      fontWeight={fontWeight}
       {...forwardProps}
     >
       {children}
